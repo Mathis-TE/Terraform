82ca334f58810df83410ab2291a5b235462b132a
Nine-rem <alexis2000.te@hotmail.fr>
2026-07-29 11:38:09 +0200

    modified kv

--- terraform/azure/keyvault.tf
+++ terraform/azure/keyvault.tf
@@ -5,10 +5,10 @@
 # ---------------------------------------------------------------------------
 data "azurerm_client_config" "current" {}
 
-resource "azurerm_key_vault" "this" {
+resource "azurerm_key_vault" "kv" {
   name                = "kv-${local.name_prefix}"
-  resource_group_name = azurerm_resource_group.this.name
-  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.kv.name
+  location            = azurerm_resource_group.kv.location
   tenant_id           = data.azurerm_client_config.current.tenant_id
   sku_name            = "standard"
 
@@ -27,11 +27,11 @@ resource "azurerm_key_vault" "this" {
 resource "azurerm_key_vault_secret" "acr_admin_password" {
   name         = "acr-admin-password"
   value        = azurerm_container_registry.this.admin_password
-  key_vault_id = azurerm_key_vault.this.id
+  key_vault_id = azurerm_key_vault.kv.id
 }
 
 resource "azurerm_key_vault_secret" "storage_sas_token" {
   name         = "storage-sas-token"
   value        = data.azurerm_storage_account_sas.models_ro.sas
-  key_vault_id = azurerm_key_vault.this.id
+  key_vault_id = azurerm_key_vault.kv.id
 }

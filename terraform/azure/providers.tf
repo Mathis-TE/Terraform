ebaaa214dd0261a7f9ebb5d9342e13d8567ea4e0
Nine-rem <alexis2000.te@hotmail.fr>
2026-07-29 13:15:24 +0200

    providers

--- terraform/azure/providers.tf
+++ terraform/azure/providers.tf
@@ -7,17 +7,6 @@ terraform {
       version = "~> 3.100"
     }
   }
-
-  # État Terraform stocké en local par défaut (terraform.tfstate, gitignored).
-  # Pour un travail en équipe, migrer vers un backend distant une fois le
-  # storage account créé (voir docs/azure-setup.md) :
-  #
-  # backend "azurerm" {
-  #   resource_group_name  = "rg-estimia-tfstate"
-  #   storage_account_name = "<nom-unique>"
-  #   container_name       = "tfstate"
-  #   key                  = "estimia.tfstate"
-  # }
 }
 
 provider "azurerm" {
